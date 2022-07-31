0a31cbdf8eb46e6dccd9914c823b3d70d12b5ce2
Axel Dovskog <axel.dovskog@outlook.com>
2022-07-31 21:43:28 +0200

    Restructuring

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import numpy as np
-from nnfs.datasets import spiral_data
-import nnfs
 from activation_functions import ReLU, Softmax
+from layers import Dense
 
-nnfs.init()
+def main():
+    pass
 
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
 
-X, y = spiral_data(samples=100, classes=3)
-
-dense1 = Layer_Dense(2,3)
-activation1 = ReLU()
-
-dense2 = Layer_Dense(3, 3)
-activation2 = Softmax()
-
-dense1.forward(X)
-activation1.forward(dense1.output)
-
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-
-print(activation2.output[:5])
+if __name__ == '__main__':
+    main()

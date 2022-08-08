503210a66f50904e1dea3a820465d7ec1e9f98f6
Axel Dovskog <axel.dovskog@outlook.com>
2022-08-08 23:23:17 +0200

    Begun working on data

--- data.py
+++ data.py
@@ -8,3 +8,19 @@ BATCH = NamedTuple("BATCH", [("inputs", Tensor), ("targets", Tensor)])
 class DataIterator:
     def __call__(self, inputs: Tensor, targets: Tensor) -> Iterator[BATCH]:
         raise NotImplementedError
+
+
+class BatchIterator:
+    def __init__(self, batch_size: int = 32, shuffle: bool = True) -> None:
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+    def __call__(self, inputs: Tensor, targets: Tensor) -> Iterator[BATCH]:
+        starts = np.arange(0, len(inputs), self.batch_size)
+        if self.shuffle:
+            np.random.shuffle(starts)
+
+        for start in starts:
+            end = start + self.batch_size
+            batch_inputs = inputs[start:end]
+            batch_targets = targets[start:end]
+            yield BATCH[batch_inputs, batch_targets]
